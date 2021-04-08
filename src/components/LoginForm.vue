9f7431794615fde423071b36ad667d2a7950dcbf
Federico Morozzo <fm0007@belgrid.net>
2021-04-08 11:54:15 +0200

    login signup without connection to firebase

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+      <input type="email" required placeholder="email" v-model="email">
+      <input type="password" required placeholder="Password" v-model="password">
+      <button>Log in</button>
+
+      <p>Don't have an account? <a href="">Sign Up</p>
+  </form>
+</template>
+
+p<script>
+import { ref } from '@vue/reactivity'
+export default {
+
+    setup(){
+        const email = ref('')
+        const password = ref('')
+   
+        const handleSubmit = () => {
+            console.log(email.value,password.value)
+        }
+        return {email, password, handleSubmit}
+    }
+}
+</script>
+
+<style>
+
+</style>
