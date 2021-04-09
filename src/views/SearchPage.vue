e89805779faf1f07cf2c38fa80938a924a9764dd
Federico Morozzo <fm0007@belgrid.net>
2021-04-09 22:37:46 +0200

    Little bg CSS fix

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="py-10 bg-gray-100">
+  <div class="py-10">
     <header>
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h1 class="text-3xl font-bold leading-tight text-gray-900">
