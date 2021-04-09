1801187a8b0b30e92df01d1a4b713fc18ea1a342
Federico Morozzo <fm0007@belgrid.net>
2021-04-09 22:22:15 +0200

    Search functionnalities improved : search by field

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -8,11 +8,10 @@
                       <button @click="key()" class="outline-none focus:outline-none"><svg class=" w-5 text-gray-600 h-5 cursor-pointer" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg></button>
                       <input type="search" name="" id="" @keydown.enter="key()" placeholder="search for images" v-model="query" class="w-full pl-4 text-sm outline-none focus:outline-none bg-transparent">
                       <div class="select">
-                        <select name="" id="" x-model="image_type" class="text-sm outline-none focus:outline-none bg-transparent">
-                          <option value="all" selected>All</option>
-                          <option value="photo">Artists</option>
-                          <option value="illustration">Titles</option>
-                          <option value="vector">Label</option>
+                        <select name="" id="" v-model="searchField" class="text-sm outline-none focus:outline-none bg-transparent">
+                          <option value="Artist" selected >Artists</option>
+                          <option value="Title">Titles</option>
+                          <option value="Label">Label</option>
                          </select>
                       </div>
                     </div>
@@ -37,9 +36,9 @@ export default {
         apiKey: 'keyQKXuzuIIC7c8Ph', // Always use a read-only account token
         appID:'app0Z70uYtVAdttHi',
         base: '/Records',
-        sort: '&sort%5B0%5D%5Bfield%5D=Artist&sort%5B0%5D%5Bdirection%5D=asc',
+        sort: 'sort%5B0%5D%5Bdirection%5D=asc&sort%5B0%5D%5Bfield%5D=',
         formula: '',
-        message:'',
+        searchField:'',
         query:'',
         url:'',
         records:{}
@@ -49,7 +48,7 @@ export default {
     methods: {
         getData: function () {
             axios({
-                url: this.apiUrl + this.appID + this.base + '?' +this.formula + '&' + this.sort,
+                url: this.apiUrl + this.appID + this.base + '?' +this.formula + '&' + this.sort + this.searchField,
                 headers: {
                     'Authorization': `Bearer ${this.apiKey}`
                 }
@@ -59,7 +58,7 @@ export default {
             });
         },
         key: function(event) {
-            this.formula = 'filterByFormula=SEARCH(LOWER(%22' + this.query + '%22)%2C+LOWER(Artist))',
+            this.formula = 'filterByFormula=SEARCH(LOWER(%22' + this.query + '%22)%2C+LOWER(' + this.searchField + '))',
             console.log(this.apiUrl + this.appID + this.base + '?' +this.formula + '&' + this.sort)
             this.getData()
         }
